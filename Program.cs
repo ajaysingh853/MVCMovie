8dc53ac3fe64fa6c4d0dbd9664d62a80f1af8695
ajaysingh853@gmail.com <ajay.singh@EMEDITEK.NET>
2019-10-17 19:19:47 +0530

    new

--- Program.cs
+++ Program.cs
@@ -7,14 +7,41 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
     public class Program
     {
+        // public static void Main(string[] args)
+        // {
+        //     CreateWebHostBuilder(args).Build().Run();
+
+        // }
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
