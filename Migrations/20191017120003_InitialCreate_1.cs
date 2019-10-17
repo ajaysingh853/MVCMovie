55010b3e0b393a9927db5aff56981931ce85b930
ajaysingh853@gmail.com <ajay.singh@EMEDITEK.NET>
2019-10-17 17:36:32 +0530

    Migration

---
+++ Migrations/20191017120003_InitialCreate_1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class InitialCreate_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Movie");
+        }
+    }
+}
